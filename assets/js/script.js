506f449a39a307774e7a17132b865d36535c36f3
AwrelH <hiwa.awrel@gmail.com>
2022-06-15 15:28:49 +0000

    add few adjustment in cod

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,6 +72,7 @@ function calculateCorrectAnswer() {
     } else if (operator === '-') {
         return [operand1 - operand2, 'subtract'];
     }  else if (operator === '/') {
+
         return [operand1 / operand2, 'division'];
     } else {
         alert(`Unimplemented operator ${operator}`);
@@ -104,8 +105,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 :  operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 :  operand1;
     document.getElementById('operator').textContent = '-';
 }
 
@@ -116,7 +117,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 :  operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 :  operand1;
     document.getElementById('operator').textContent = '/';
 }
