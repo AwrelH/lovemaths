5f19c822a0656a66f12db318a1738f4c3b468d99
AwrelH <hiwa.awrel@gmail.com>
2022-06-15 08:46:27 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,36 @@
-
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     let buttons = document.getElementsByTagName('button');
 
-    for(let button of buttons) {
-        button.addEventListener('click', function() {
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
             if (this.getAttribute('data-type') === 'submit') {
                 alert('You clicked Submit!');
-            }
-            else {
+            } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
-        })
+        });
     }
-
-})
+    runGame('addition');
+});
 
 /**
  * the main 'loop', called when the script is first loaded
  * and after the user's answer has been processed
  */
 
-function runGame() {    
-    //creates two random numbers 1-25
-    let num1 = Math.floor(Math.random()*25)+1;
-    let num2 = Math.floor(Math.random()*25)+1;
+function runGame(gameType) {
 
+    //creates two random numbers 1-25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -33,7 +38,7 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
-    
+
 }
 
 function incrementScore() {
@@ -44,8 +49,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
